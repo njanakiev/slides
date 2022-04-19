148068ca6ee7f39da8dacf97354d17cc6aa8a8ba
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2022-04-19 22:24:11 +0200

    Update slides

--- scalable-geospatial-data-science/build_reveal_slides.sh
+++ scalable-geospatial-data-science/build_reveal_slides.sh
@@ -1,3 +1,4 @@
-jupyter-nbconvert --to slides presentation.ipynb --reveal-prefix=reveal.js
+jupyter-nbconvert --to slides presentation.ipynb --reveal-prefix=../reveal.js
 mv presentation.slides.html index.html
-
+sed -i 's#<title>presentation slides</title>#<title>Scalable Geospatial Data Science</title>#g' \
+  index.html
